0a87b0315aeeb06f2f8841cc30fbd0777125db85
ramawm20 <ramawael54@gmail.com>
2023-05-06 23:37:20 +0000

    Lab 15 has been solved

---
+++ schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS favoriteMovie;
+
+
+CREATE TABLE IF NOT EXISTS favoriteMovie (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    summary VARCHAR(255),
+    years VARCHAR(255)
+);
